1c9c7428d0b36c563b1381086d7ca3f96a946b43
Parth Sharma <parthsharma8421@gmail.com>
2023-12-30 13:10:08 +0530

    Added zip_code to properties and profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_065008) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_30_072959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_065008) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -41,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_065008) do
     t.string "address_2"
     t.float "latitude"
     t.float "longitude"
+    t.string "zip_code"
     t.index ["latitude", "longitude"], name: "index_properties_on_latitude_and_longitude"
   end
 
