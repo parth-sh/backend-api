f9f3f4cd26e7b0502aa24e9d835c6fad662b0bb6
Parth Sharma <parthsharma8421@gmail.com>
2023-12-30 18:30:12 +0530

    wip

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -2,6 +2,6 @@ class Api::ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    render json: current_user.profile
+    render json: current_user.profile.to_json(:except => [:id, :user_id, :created_at, :updated_at])
   end
 end
